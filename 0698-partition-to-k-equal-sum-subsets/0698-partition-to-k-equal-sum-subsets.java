5e0d13cce6e598a84cdc182cb5cc26c65287b981
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-10-06 11:21:10 +0530

    Time: 264 ms (56.74%), Space: 44 MB (17.05%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,38 @@
+class Solution {
+    private final List<Set<Integer>> allSubsets = new ArrayList<>();
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = Arrays.stream(nums).sum();
+        if(sum % k != 0) return false;
+        getAllSubsets(nums.length, sum / k, new HashSet<>(), nums, false);
+        return allSubsets.size() >= k && canPartition(allSubsets.size(), k, nums.length, new HashSet<>());
+    }
+
+    private boolean canPartition(int n, int k, int size, Set<Integer> current) {
+        if(k == 0 && current.size() == size) return true;
+        if(n == 0 || k < 0) return false;
+        boolean addSet = false;
+        if(allUnique(current, allSubsets.get(n-1))) {
+            current.addAll(allSubsets.get(n - 1));
+            addSet = canPartition(n - 1, k - 1, size, current);
+            current.removeAll(allSubsets.get(n - 1));
+        }
+        return addSet || canPartition(n - 1, k, size, current);
+    }
+
+    private void getAllSubsets(int n, int targetSum, Set<Integer> subsets, int[] nums, boolean lol) {
+        if(targetSum == 0) {
+            allSubsets.add(new HashSet<>(subsets));
+            return;
+        }
+        if (n == 0 || targetSum < 0) return;
+        subsets.add(n-1);
+        getAllSubsets(n-1, targetSum - nums[n-1], subsets, nums, true);
+        subsets.remove(n-1);
+        getAllSubsets(n-1, targetSum, subsets, nums, false);
+    }
+    
+    private boolean allUnique(Set<Integer> set1, Set<Integer> set2) {
+        for (Integer num: set1) if(set2.contains(num)) return false;
+        return true;
+    }
+}
